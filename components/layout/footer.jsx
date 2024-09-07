61ef62bba66ff5e5c855ca0260a403f564415a3f
David <debukali@gmail.com>
2024-09-07 11:32:23 +0300

    Remove redundant html from footer

--- components/layout/footer.jsx
+++ components/layout/footer.jsx
@@ -46,17 +46,7 @@ export default function Footer() {
 						}
 					</ul>
 					<ul className={css.links}>
-						<li><h4>Links</h4></li>
-						{
-						content.links.map( ({ person, link, note }, index) => {
-							return (
-								<li key={index}>
-									<a href={link} rel="noreferrer" target="_blank">{person} <Icon icon={[ 'fad', 'arrow-up-right-from-square' ]} /></a>
-									<p>{note}</p>
-								</li>
-							)
-						})
-						}
+						
 					</ul>
 					<ul className={css.social}>
 						<li><h4>Social</h4></li>
@@ -74,14 +64,6 @@ export default function Footer() {
 				<section className={css.github}>
 					<a href={settings.portfolio.repo_html} rel="noreferrer" target="_blank">
 						<h5>{settings.portfolio.forkthis}</h5>
-						<ul>
-							<li>
-								<p><Icon icon={[ 'fad', 'code-branch' ]} /> Forks: { gitHubInfo.forks }</p>
-							</li>
-							<li>
-								<p><Icon icon={[ 'fad', 'star' ]} /> Stars: { gitHubInfo.stars }</p>
-							</li>
-						</ul>
 					</a>
 				</section>
 			</Container>
