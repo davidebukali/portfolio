50bf98b5c33ed0d7aee0647b61625f861fe720d1
David <debukali@gmail.com>
2024-10-04 15:30:48 +0300

    Fix articles failing to load

--- components/sections/articles/recent.jsx
+++ components/sections/articles/recent.jsx
@@ -1,17 +1,18 @@
 import Section 		from '../../structure/section';
 import Container 	from '../../structure/container';
-
-import Image from 'next/image'
 import SectionTitle from '../../blocks/section.title.block'
-
 import Icon from '../../utils/icon.util'
-
 import css from '../../../styles/sections/articles/recent.module.scss'
+import { useState, useEffect } from 'react'
 
 export default function Recent({ mediumArticles }) {
+	const [articles, setArticles] = useState([])
 
+	useEffect(() => {
+		setArticles(mediumArticles.items)
+	}, [mediumArticles])
+	
 	const feed 		= mediumArticles.feed
-	const articles 	= mediumArticles.items
 
 	return (
 		<Section classProp="borderBottom">
